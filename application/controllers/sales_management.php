cb50fa803afcaf22c9e2e7f3aee1be233f999b10
Javed101 <javedahamad4@gmail.com>
2015-07-07 17:26:48 +0530

    Added new module sales management

---
+++ application/controllers/sales_management.php
@@ -0,0 +1,76 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//error_reporting(0);
+class sales_management extends CI_Controller {
+ public function __construct()
+ {
+  parent::__construct();
+  $this->load->model('sales_model');
+  $this->load->helper(array('form','url'));
+ }
+ public function add_lead()
+{
+
+    $data['title']='Add Leads';
+      $this->load->view('templates/header.php',$data);
+      $this->load->view('pages/add_lead.php');
+      $this->load->view('templates/footer.php');
+
+}
+public function do_add_lead()
+{
+          
+          if($this->sales_model->add_lead())
+         {
+           $data['msg']="Lead Aded Added";
+           $this->load->view('templates/header.php');
+           $this->load->view('pages/add_lead.php',$data);
+           $this->load->view('templates/footer.php');
+         }
+  
+}
+
+
+  public function view_lead()
+  {
+      $data['title']='Lead  Details';
+      $data['h']=$this->sales_model->view_leads_details();
+      $this->load->view('templates/header.php',$data);
+      $this->load->view('pages/view_lead.php',$data);
+      $this->load->view('templates/footer.php'); 
+  }
+
+  public function editlead($id)
+{
+
+  $this->load->database();
+  $data['title']='Update Lead Details';
+  $data['h']=$this->sales_model->editlead($id);
+  $this->load->view('templates/header.php',$data);
+  $this->load->view('pages/edit_lead.php',$data);
+  $this->load->view('templates/footer.php');
+}
+
+public function do_edit_lead($id)
+{
+  $this->load->database();
+  if($this->sales_model->do_editlead($id))
+  {
+    redirect(base_url().'sales_management/view_lead');
+  }
+  else{
+    echo "record not updated";
+  }
+
+}
+
+public function deletelead($id)
+{
+  if($this->sales_model->deletelead($id))
+  {
+    redirect(base_url().'sales_management/view_lead');
+  }
+}
+
+
+
+}
---
+++ application/models/sales_model.php
@@ -0,0 +1,67 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class sales_model extends CI_Model {
+public function __construct()
+{
+parent::__construct();
+//$this->load->library('session');
+	$this->load->database();
+}
+
+	
+	public function add_lead()
+	{
+		$data=array(
+		'lead_name'=>$this->input->post('lead_name'),
+		'lead_generate_date'=>$this->input->post('lead_generate_date'),
+		'lead_source'=>$this->input->post('lead_source'),
+		'lead_service'=>$this->input->post('phone')
+		);
+		$this->db->insert('leads',$data);
+		return true;	
+	}
+
+	public function view_lead_details()
+	{
+		$query = $this->db->get('leads');  
+    	return $query;
+	}
+
+	public function editlead($id)
+	{
+		
+		$this->db->where("lead_id",$id);
+  		$query=$this->db->get("leads");
+  		return $query->result();
+	}
+
+	public function do_editlead($id)
+	{
+
+		$data=array(
+		'lead_name'=>$this->input->post('lead_name'),
+		'lead_generate_date'=>$this->input->post('lead_generate_date'),
+		'lead_source'=>$this->input->post('lead_source'),
+		'lead_service'=>$this->input->post('phone')
+		);
+		$this->db->where('lead_id', $id);
+		$this->db->update('leads',$data);
+		return true;
+			
+	}
+
+	public function deletelead($id)
+	{
+		$this->db->where("lead_id",$id);
+  		if($this->db->delete("leads"))
+  		{
+  			return true;
+  		}
+
+
+	}
+
+	
+
+
+
+}
