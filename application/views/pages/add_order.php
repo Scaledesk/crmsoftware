782eb5bff308372713575280ec15572c831fd828
vinay2 <vk324005@gmail.com>
2015-07-09 11:39:38 +0530

    add new code in Add_order form

--- application/views/pages/add_order.php
+++ application/views/pages/add_order.php
@@ -2,60 +2,74 @@
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
 
-
-
-
-
 <div id="main-wrapper" class="container">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="panel panel-white">
                                 <div class="panel-body">
-                                    <center><h3>Add Order</h3></center>
-                                    <div class="col-md-6">
-                                    <form >
-                                        <div class="form-group">
+                                  <center><label><h2>Add Order</h2></label></center>  
+                                <div class="row m-b-lg">
+                                    <form id="wizardForm" action="<?=(base_url().'sales_management/do_add_order')?>" method="post">
+                                <div class="col-md-6">
+                                    
+                                        <div class="form-group col-md-12">
                                             <label>Title</label>
-                                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Title">
+                                            <input type="text" class="form-control" id="exampleInputEmail1" name="order_title" placeholder="Title" required="">
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group col-md-12">
                                             <label>Name of Client</label>
-                                            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Name of Client">
+                                            <input type="text" class="form-control" id="exampleInputPassword1" name="client_name" placeholder="Name of Client" required="">
                                         </div>
                                         
-                                        <div class="form-group">
+                                        <div class="form-group col-md-12">
                                             <label>Order Cast</label>
-                                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Order Cast">
+                                            <input type="text" class="form-control" id="exampleInputEmail1" name="order_cast" placeholder="Order Cast" required="">
                                         </div>
 
                                       </div> 
-                                      <div class="col-md-6">
+                                  
 
-                                        <div class="form-group">
-                                            <label>Date Of Starting</label>
-                                            <input type="text" class="form-control" id="exampleInputEmail1" name="date1" placeholder="Date Of Starting">
+
+
+
+
+
+
+
+
+                                        <div class="col-md-6">
+
+                                            
+                                        <div class="form-group col-md-12">
+                                            <label>Starting date</label>
+                                            <input type="text" class="form-control" id="exampleInputEmail2" name="date1" placeholder="Enter Your Starting" required="">
                                         </div>
-                                        <div class="form-group">
-                                            <label>Date Of Praposed Ending</label>
-                                            <input type="text" class="form-control" id="exampleInputEmail1" name="date2" placeholder="Date Of Praposed Ending">
+
+                                        <div class="form-group col-md-12">
+                                            <label>Ending Date</label>
+                                            <input type="text" class="form-control" id="exampleInputPassword2" name="date2" placeholder="Enter Your Ending date" required="">
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group col-md-12">
                                             <label>Lead Name</label>
-                                            <select  class="form-control" id="exampleInputEmail1">
+                                            <select  class="form-control" id="exampleInputEmail1" name="lead_id" required="">
                                                 <option>select Lead Name</option></select>
                                         </div>
 
-                                      </div> 
-                                        <center><button type="submit" class="btn btn-primary">Submit</button></center>
-                                    
-                                            <script type="text/javascript">
+                                        
+                                   
+                                        </div>
+                                        <center><button type="submit" class="btn btn-info">Submit</button></center>
+
+                                        <script type="text/javascript">
                                  $('input[name~=date1],input[name~=date2]').each(function(){
                                         $(this).datepicker({dateFormat:'yy-mm-dd'});
                                     });</script>
 
-                                    </form>
-                                
-                            </div>
+
+                                </form>
+
+                                    </div>
+                                    </div>
                             </div>
                         </div>
                     </div><!-- Row -->
