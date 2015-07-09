5afa2857b19121c0900e62d8aac9f61b5d45795a
vinay2 <vk324005@gmail.com>
2015-07-09 15:52:39 +0530

    add the code in Manage invoice and order invoice

---
+++ application/views/pages/add_invoice.php
@@ -0,0 +1,51 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-white">
+                                <div class="panel-body">
+                                  <center><label><h2>Add invoice</h2></label></center>  
+                                <div class="row m-b-lg">
+        
+                                <div class="col-md-8 center">
+                                    
+                                        <div class="form-group col-md-12">
+                                            <label>Select order id</label>
+                                            <select  class="form-control" id="exampleInputEmail1" name="lead_id" required="">
+                                                <option value="0">-select-</option>
+                                                
+                                            </select>
+                                        </div>
+                                        </div> 
+                                  
+
+
+
+
+
+
+
+                                    <!--containner-start-->
+
+                                        <div class="col-md-8 center">
+
+                                            
+                                        <div class="form-group col-md-12">
+
+                                            Start Matter here
+                                        </div>
+                                            
+                                        </div>
+
+
+                                        </div>
+
+
+                                        <!--container-end-->
+                                                           
+
+                                    </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
--- application/views/pages/manage_invoice.php
+++ application/views/pages/manage_invoice.php
@@ -3,10 +3,10 @@
                         <div class="col-md-12">
                             <div class="panel panel-white">
                                 <div class="panel-body">
-                                  <center><label><h2>Manage_invoice</h2></label></center>  
+                                  <center><label><h2>Manage invoice</h2></label></center>  
                                 <div class="row m-b-lg">
         
-                                <div class="col-md-12">
+                                <div class="col-md-8 center">
                                     
                                         <div class="form-group col-md-12">
                                             <label>Select order invoice</label>
@@ -24,20 +24,23 @@
 
 
 
+                                    <!--containner-start-->
 
-
-                                        <div class="col-md-6">
+                                        <div class="col-md-8 center">
 
                                             
                                         <div class="form-group col-md-12">
+
+                                            Start Matter here
+                                        </div>
                                             
                                         </div>
 
-                                        
 
-                                        
-                                   
                                         </div>
+
+
+                                        <!--container-end-->
                                                            
 
                                     </div>
