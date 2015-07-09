dca59a55a344fcf3ff19eab1f904984ce746a93d
niteshbca <singhniteshbca@gmail.com>
2015-07-09 15:44:05 +0530

    added calendar page

---
+++ application/views/pages/calendar.php
@@ -0,0 +1,50 @@
+            
+
+
+            <div class="page-inner">
+                <div class="page-breadcrumb">
+                    <ol class="breadcrumb container">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="#">Extra</a></li>
+                        <li class="active">Calendar</li>
+                    </ol>
+                </div>
+                <div class="page-title">
+                    <div class="container">
+                        <h3>Calendar</h3>
+                    </div>
+                </div>
+                <div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="panel panel-white">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Events</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="events">
+                                        <div class="calendar-event"><p>Meeting with Ann.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
+                                        <div class="calendar-event"><p>Go Shopping.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
+                                        <div class="calendar-event"><p>finish #7 project.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
+                                        <div class="calendar-event"><p>Meeting With Mike.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
+                                        <div class="calendar-event"><p>Meeting with Joe.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
+                                    </div>
+                                    <input class="form-control add-event" type="text" placeholder="Add Event">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="panel panel-white">
+                                <div class="panel-body">
+                                    <div id="calendar"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
+                <div class="page-footer">
+                    <div class="container">
+                        <p class="no-s">2015 &copy; Modern by Steelcoders.</p>
+                    </div>
+                </div>
+            </div>
