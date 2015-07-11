7dd4b9321ee5151dc437310d4883dc53efda334b
vinay2 <vk324005@gmail.com>
2015-07-11 12:44:48 +0530

    delete the code in datepicker debug

--- application/views/pages/add_lead.php
+++ application/views/pages/add_lead.php
@@ -1,17 +1,6 @@
 
 		
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-        <link rel="stylesheet" href="/resources/demos/style.css"> 
 
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
-  
-  <script>
-  $(function() {
-    $( "#datepicker1,#datepicker2,#datepicker3" ).datepicker({ dateFormat: 'yy-mm-dd' });
-  });
-  </script>
 
 
 
