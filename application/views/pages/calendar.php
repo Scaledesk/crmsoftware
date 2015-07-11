1e635f5e27a034d3b25031d78c94d6baa0dad133
vinay2 <vk324005@gmail.com>
2015-07-11 14:46:09 +0530

    add new code for calendar

--- application/views/pages/calendar.php
+++ application/views/pages/calendar.php
@@ -1,50 +1,89 @@
-            
-
-
-            <div class="page-inner">
-                <div class="page-breadcrumb">
-                    <ol class="breadcrumb container">
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="#">Extra</a></li>
-                        <li class="active">Calendar</li>
-                    </ol>
-                </div>
-                <div class="page-title">
-                    <div class="container">
-                        <h3>Calendar</h3>
-                    </div>
-                </div>
-                <div id="main-wrapper" class="container">
+<div id="main-wrapper" class="container">
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-12">
                             <div class="panel panel-white">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Events</h3>
-                                </div>
                                 <div class="panel-body">
-                                    <div class="events">
-                                        <div class="calendar-event"><p>Meeting with Ann.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
-                                        <div class="calendar-event"><p>Go Shopping.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
-                                        <div class="calendar-event"><p>finish #7 project.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
-                                        <div class="calendar-event"><p>Meeting With Mike.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
-                                        <div class="calendar-event"><p>Meeting with Joe.</p><a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-remove"></i></a></div>
+                                                              
+                                <center><label><h2>Add Event</h2></label></center>  
+                                <div class="row m-b-lg">
+                                <form id="wizardForm" action="" method="post">
+                           
+                                        <div class="col-md-6">
+                                    
+                                        <div class="form-group col-md-12">
+                                            <label>Start Date</label>
+                                            <input type="text" class="form-control date-picker" name="" placeholder="Start Date" required="">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label>End Date</label>
+                                            <input type="text" class="form-control date-picker" name="" placeholder="End Dtae" required="">
+                                        </div>
+                                        
+                                        <div class="form-group col-md-12">
+                                            <label>Title</label>
+                                            <input type="text" class="form-control" id="" value="" name="" placeholder="Title" required>
+                                        </div>
+
+                                      </div> 
+                                  
+
+
+
+
+
+
+
+
+
+                                        <div class="col-md-6">
+
+                                            
+                                        
+
+                                        <div class="form-group col-md-12">
+                                            <label>Start Date</label>
+                                            <input type="text" class="form-control date-picker" name="" placeholder="Start Date" required="">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label>End Date</label>
+                                            <input type="text" class="form-control date-picker" name="" placeholder="End Dtae" required="">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label>Event Search</label>
+                                        <div class="input-group">
+                                            <input type="text" name="search" class="form-control input-search" placeholder="Event Search...">
+                                            <span class="input-group-btn">
+                                                <button class="btn btn-success" type="button"><i class="fa fa-search"></i></button>
+                                            </span>
+                                        </div><!-- Input Group -->
                                     </div>
-                                    <input class="form-control add-event" type="text" placeholder="Add Event">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="panel panel-white">
-                                <div class="panel-body">
-                                    <div id="calendar"></div>
-                                </div>
+
+                                        
+                                   
+                                        </div>
+                                        <div class="col-md-12">
+                                        <div class="form-group col-md-12"
+                                        <label class="sr-only">Description</label>
+                                            <textarea placeholder="Description" class="note-codable"  style="margin: 0px; width: 1069px; height: 93px;">                                            
+                                            
+                                            
+                                            </textarea>
+                                            </div>
+                                        
+                                        </div>
+                                        
+                                        
+                                       
+                                        
+                                            <center><button type="submit" class="btn btn-info">Add Event</button></center>       
+                                      
+                                       
+
+                                </form>
+
+                                    </div>
+                                    </div>
                             </div>
                         </div>
                     </div><!-- Row -->
                 </div><!-- Main Wrapper -->
-                <div class="page-footer">
-                    <div class="container">
-                        <p class="no-s">2015 &copy; Modern by Steelcoders.</p>
-                    </div>
-                </div>
-            </div>
