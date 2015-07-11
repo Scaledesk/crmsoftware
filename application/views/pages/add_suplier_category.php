83c41152a9acb508019b16daf52dcf228fb5635c
vinay2 <vk324005@gmail.com>
2015-07-11 17:36:35 +0530

    change the form code in supplier category

--- application/views/pages/add_suplier_category.php
+++ application/views/pages/add_suplier_category.php
@@ -1,33 +1,35 @@
 <div id="main-wrapper" class="container">
                     <div class="row">
-                        <div class="col-md-8 center">
+                        <div class="col-md-10 center">
                             <div class="panel panel-white">
                                 <div class="panel-body">
                                                               
                                 <center><label><h2>Add Supplier Category</h2></label></center>  
-                                <div class="row">
+                                <div class="row m-b-lg">
                                     <form id="wizardForm" action="" method="post">
                            
-                                         <div class="col-md-10 center">
+                                <div class="col-md-6 center">
                                     
                                         <div class="form-group col-md-12">
                                             <label>Category Name</label>
-                                            <input type="text" class="form-control" id="Category_Name" name="Category_Name" placeholder="Category Name" required="">
+                                            <input type="text" class="form-control" name="Category_Name" id="Category_Name" placeholder="Category Name" required="">
                                         </div>
-
                                         <div class="form-group col-md-12">
-                                        <label for="exampleInputEmail"> Category Description</label>
-                                        <textarea class="form-control" placeholder="Category Description" id="Category_Description" name="Category_Description" required="" style="margin: 0px -0.078125px 0px 0px; height: 160px; width: 555px;"></textarea>
-                                        </div>   
-										<div class="form-group col-md-4 center">
-								      <center><input type="submit" class="btn btn-primary btn-block" name="" value="Add"></center> 
-									  </div>
-									</div> 
-                                </form>
+                                        <label> Category Description</label>
+                                        <textarea class="form-control" placeholder="Category Description" id="Category_Description" name="Category_Description" required="" style="margin: 0px -0.078125px 0px 0px; height: 160px; width: 406px;"></textarea>
+                                        </div>
+                                        
+                                        <center><input type="submit" class="btn btn-success btn-block m-t-xs" value="Add"></center>
+                                        <style>
+                                        input[type=submit].btn-block {  width: 35%;}</style>
+                                        
 
+                                      </div> 
+                                    </form>
+
+                                    </div>
+                                    </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div><!-- Row -->
-        </div><!-- Main Wrapper -->
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
