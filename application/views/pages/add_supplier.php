8d8cf9b7b9c434894e40e86ab9fde79129ba08b2
vinay2 <vk324005@gmail.com>
2015-07-13 11:10:33 +0530

    add_supplier form in pages

---
+++ application/views/pages/add_supplier.php
@@ -0,0 +1,92 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-10">
+                            <div class="panel panel-white">
+                                <div class="panel-body">
+                                     <div id="rootwizard">
+                                        <ul class="nav nav-tabs" role="tablist">
+                                            <li role="presentation" class="active"><a href="#tab1" data-toggle="tab"><i class="fa fa-user m-r-xs"></i><b>Add Supplier</b></a></li>
+                                            
+                                        </ul>
+                          
+                                    
+                                        
+                                        <form id="wizardForm">
+                                            <div class="tab-content">
+                                                <div class="tab-pane active fade in" id="tab1">
+                                                    <div class="row m-b-lg">
+                                                        <div class="col-md-6">
+                                                            <div class="row">
+                                                                <div class="form-group col-md-12">
+                                                                    <label>Supplier Name</label>
+                                                                    <input type="text" class="form-control" name="Supplier_Name" id="Supplier_Name" placeholder="Supplier Name" required="">
+                                                                </div>
+                                                                
+                                                                
+                                                                <div class="form-group col-md-12">
+																	<label> Supplier Address</label>
+																	<textarea class="form-control" placeholder="Supplier Address" id="supplier_address" name="supplier_address" required="" style="margin: 0px -0.078125px 0px 0px; height: 100px; width: 420px;"></textarea>
+																</div>
+                                                                <div class="form-group col-md-12">
+                                                                    <label>Supplier Phone1</label>
+                                                                    <input type="number" class="form-control" name="supplier_phone1" id="supplier_phone1" placeholder="Supplier Phone1" required="">
+                                                                </div>
+                                                                <div class="form-group col-md-12">
+                                                                    <label>Supplier Phone2</label>
+                                                                    <input type="number" class="form-control" name="supplier_phone2" id="supplier_phone2" placeholder="Supplier Phone2" required="">
+                                                                
+																</div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            
+                                                                <div class="form-group col-md-12">
+                                                                    <label>Supplier Email</label>
+                                                                    <input type="email" class="form-control" name="supplier_email" id="supplier_email" placeholder="Supplier Email" required="">
+                                                                </div>
+                                                                <div class="form-group col-md-12">
+                                                                    <label>Supplier Fax</label>
+                                                                    <input type="text" class="form-control" name="supplier_fax" id="supplier_fax" placeholder="Supplier Fax" required="">
+                                                                </div>
+                                                                <div class="form-group col-md-12">
+																	<label> Supplier Description</label>
+																	<textarea class="form-control" placeholder="Supplier Description" id="supplier_description" name="supplier_description" required="" style="margin: 0px -0.078125px 0px 0px; height: 100px; width: 392px;"></textarea>
+																</div>
+																<div class="form-group col-md-12">
+                                                                    <label>Category Id</label>
+                                                                    <select class="form-control" placeholder="Category Id" required="">
+																	<option>Select Category</option>
+																	
+																	<option>--1--</option>
+																	<option>--2--</option>
+																	</select>
+																	
+																	
+																	
+																	
+                                                                </div>
+                                                                
+                                                                
+                                                            
+                                                            
+                                                        </div>
+                                                        
+
+
+                                                    </div>
+
+                                                </div>
+                                                
+                                                
+                                                
+                                                <div class="form-group col-md-4 center">
+                                                    <button type="submit" class="btn btn-primary btn-block">Add Lead</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
