3761f70e493d9aa6a29975a1e4ffcae824dddd00
vinay2 <vk324005@gmail.com>
2015-07-14 18:02:38 +0530

    add form and code in pages(add_knowedge_category.php)

---
+++ application/views/pages/add_knowedge_category.php
@@ -0,0 +1,31 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-6 center">
+                            <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">Add Knowledge Base Category </h4>
+                                </div>
+                                <div class="panel-body">
+                                    <form action="" method="post">
+									<label>Select Parent Id</label>
+									<div class="form-group">
+                                            					<select  class="form-control" id="" name=" ticket_resolution">
+                                                                <option value="select">Select</option>
+                                                                <option>1</option>
+                                                                <option>2</option>
+                                                				</select>
+									</div>
+                                        <div class="form-group">
+                                            <label>Category Name</label>
+                                            <input type="text" class="form-control"name="Category_name"  id="" placeholder="Category Name">
+                                        </div>
+										<div class="form-group">
+										<label>Category Description</label>
+                                                <textarea class="form-control" name="Category_description" id="" placeholder="Category Description" maxlength="500" required="" style="margin: 0px -0.171875px 0px 0px; height: 150px; width: 513px;"></textarea></div>
+                                        <input type="submit" class="btn btn-primary" value="Submit">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div>
