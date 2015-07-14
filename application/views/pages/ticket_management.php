bcf48b31805bcbb0eb9b0281cdaa294fdab19cc2
vinay2 <vk324005@gmail.com>
2015-07-14 15:24:45 +0530

    add form and code in pages (ticket_management.php)

---
+++ application/views/pages/ticket_management.php
@@ -0,0 +1,63 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                       <center> <h2>Ticket Management</h2></center>
+                        <div class="col-md-6 center">
+
+                                        <div class="col-md-6">
+                                            <form class="m-t-md" action="#">
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" name="ticket_name" id="" placeholder="Ticket Name" required="">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" name="ticket_raiser" id="" placeholder="Ticket Raised" required="">
+                                                </div>
+                                                <div class="form-group">
+                                                <textarea class="form-control" name="ticket_description" id="" placeholder="Description" maxlength='500' required=""></textarea>
+                                         
+												</div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" name="ticket_category" id="" placeholder="Ticket Category" required="">
+                                                </div>
+                                                
+                                                
+                                                
+                                            
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="m-t-md">
+                                                    
+                                                
+                                                <div class="form-group">
+                                                <textarea class="form-control" name="ticket_admin_comment" id="" placeholder="Admin Comments" maxlength='500' required=""></textarea>
+                                         		</div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control date-picker" name="ticket_raiser_date" id="" placeholder="Ticket Raised date" required="">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control date-picker" name="Ticket_closed_date" id="" placeholder="Ticket closed date" required="">
+                                                </div>
+                                                <div class="form-group">
+                                            
+                                           
+															<select  class="form-control" id="" name=" ticket_resolution">
+                                                                <option value="select">Select</option>
+                                                                <option>Yes</option>
+                                                                <option>No</option>
+                                                
+                                                            </select>
+												</div>
+                                            </div>
+                                            </div>
+                                                <div class="col-md-12">
+                                                <input type="submit" class="btn btn-success btn-block m-t-xs" value="Add Ticket">
+                                                </div>
+                                                
+                                            </form>
+                                        
+                                    </div>
+                                    </div>
+                                
+                           
+                        
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
