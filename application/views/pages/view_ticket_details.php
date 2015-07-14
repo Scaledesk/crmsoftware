af98fdba62a265d6fca3cc8bc201d8591b526093
vinay2 <vk324005@gmail.com>
2015-07-14 16:31:17 +0530

    add form in pages(view_ticket_table.php)

---
+++ application/views/pages/view_ticket_details.php
@@ -0,0 +1,51 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                         <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">Automation Management</h4>
+                                </div>
+                                <div class="panel-body">
+                                   <div class="table-responsive">
+                                    <table id="example" class="display table" style="width: 100%; cellspacing: 0;">
+                                        <thead>
+                                            <tr>
+                                                <th>Ticket Name</th>
+                                                <th>Ticket Raised</th>
+                                                <th>Description</th>
+                                                <th>Ticket Category</th>
+                                                <th>Admin Comments</th>
+                                                <th>Ticket Raised date</th>
+                                                <th>Ticket closed date</th>
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+												<th>Ticket Name</th>
+                                                <th>Ticket Raised</th>
+                                                <th>Description</th>
+                                                <th>Ticket Category</th>
+                                                <th>Admin Comments</th>
+                                                <th>Ticket Raised date</th>
+                                                <th>Ticket closed date</th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <tr>
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011/04/25</td>
+                                                <td>$320,800</td>
+                                                <td>$320,800</td>
+                                            </tr>
+                                            
+                                            </tbody>
+                                       </table>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
