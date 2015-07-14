cff16817078407601b124ff4e9fbbadc2c9a5ccf
Javed101 <javedahamad4@gmail.com>
2015-07-14 18:37:54 +0530

    added knowledge_base_control

---
+++ application/controllers/knowledge_base_control.php
@@ -0,0 +1,71 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Knowledge_base_control extends CI_Controller {
+ public function __construct()
+ {
+  parent::__construct();
+  $this->load->model('Knowledge_base_model');
+  $this->load->helper(array('form','url'));
+ }
+ public function add_category()
+{
+
+    $data['title']='Add Category';
+      $this->load->view('templates/header.php',$data);
+      $this->load->view('pages/add_knowledge_category.php');
+      $this->load->view('templates/footer.php');
+
+}
+public function do_add_category()
+{
+  if($this->Knowledge_base_model->add_category())
+  {
+    $data['title']='Add Category';
+    $data['msg']='Category detail saved';
+	 $this->load->view('templates/header.php',$data);
+      $this->load->view('pages/add_knowledge_category.php',$data);
+      $this->load->view('templates/footer.php');
+  }
+}
+
+public function view_ticket()
+{
+      $data['title']='View  Ticket';
+      $data['h']=$this->Ticket_model->get_ticket();
+      $this->load->view('templates/header.php',$data);
+      $this->load->view('pages/view_ticket_details.php',$data);
+      $this->load->view('templates/footer.php');
+}
+
+public function edit_ticket($id)
+{
+  $this->load->database();
+  $data['title']='Update Ticket Details';
+  $data['k']=$this->Ticket_model->edit_ticket($id);
+  $this->load->view('templates/header.php',$data);
+  $this->load->view('pages/add_ticket.php',$data);
+  $this->load->view('templates/footer.php');
+}
+
+public function do_edit_ticket($id)
+{
+  $this->load->database();
+  if($this->Ticket_model->do_edit_ticket($id))
+  {
+    redirect(base_url().'Ticket_control/view_ticket');
+  }
+  else{
+    echo "server error";
+  }
+
+}
+public function delete_ticket($id)
+{
+  if($this->Ticket_model->delete_ticket($id))
+  {
+    redirect(base_url().'Ticket_control/view_ticket');
+  }
+}
+
+
+}
+
