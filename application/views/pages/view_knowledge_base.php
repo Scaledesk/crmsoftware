dd9c2fa85d21fe809fcf75f88beff54d63e6b0f1
vinay2 <vk324005@gmail.com>
2015-07-15 11:55:10 +0530

    add data table in pages (view_knowledge_base.php)

---
+++ application/views/pages/view_knowledge_base.php
@@ -0,0 +1,45 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                         <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">Add Knowledge Base Details</h4>
+                                </div>
+                                <div class="panel-body">
+                                   <div class="table-responsive">
+                                    <table id="example" class="display table" style="width: 100%; cellspacing: 0;">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                                <th>Category</th>
+                                                <th>Tags</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+												<th>Name</th>
+                                                <th>Description</th>
+                                                <th>Category</th>
+                                                <th>Tags</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <tr>
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011/04/25</td>
+                                            </tr>
+                                            
+                                            </tbody>
+                                       </table>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
