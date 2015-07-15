1d7a14afc9a6a492fa0ddedfce7086290a9ffd55
vinay2 <vk324005@gmail.com>
2015-07-15 11:31:25 +0530

    add form in pages (add_nowledge_base.php)

---
+++ application/views/pages/add_knowledge_base.php
@@ -0,0 +1,37 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-6 center">
+                            <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">Add Knowledge Base Details </h4>
+                                </div>
+                                <div class="panel-body">
+                <form action="" method="post">
+									<div class="form-group">
+                                            <label>Name</label>
+                                            <input type="text" class="form-control"name="knowledge_base_name"  id="" placeholder="Name" required="">
+                                        </div>
+										<div class="form-group">
+										<label>Description</label>
+                                                <textarea class="form-control" name="knowledge_base_description" id="" placeholder="Description" maxlength="500"  style="margin: 0px -0.171875px 0px 0px; height: 109px; width: 511px;" required=""></textarea>
+										</div>
+									<label>Category</label>
+									<div class="form-group">
+                                            					<select  class="form-control" id="" name="knowledge_base_category" required="">
+                                                                <option value="select">Select</option>
+                                                                <option>1</option>
+                                                                <option>2</option>
+                                                				</select>
+									</div>
+										<div class="form-group">
+                                            <label>Tags</label>
+                                            <input type="text" class="form-control"name="knowledge_base_tags"  id="" placeholder="Tags" required="">
+                                        </div>
+										
+                                        <input type="submit" class="btn btn-primary" value="Submit">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div>
