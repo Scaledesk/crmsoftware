e74e7b27e8541afa7c3c6310852165c62c7e3bfd
vinay2 <vk324005@gmail.com>
2015-07-16 11:24:29 +0530

    add form in pages  name of(leave table.php)

---
+++ application/views/pages/leave_table.php
@@ -0,0 +1,43 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                         <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">Employee Details</h4>
+                                </div>
+                                <div class="panel-body">
+                                   <div class="table-responsive">
+                                    <table id="example" class="display table" style="width: 100%; cellspacing: 0;">
+                                        <thead>
+                                            <tr>
+                                                <th>Start Date</th>
+                                                <th>End Date</th>
+                                                <th>Reasion for Leave</th>
+                                                <th>Action</th>
+                                               </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+												<th>Start Date</th>
+                                                <th>End Date</th>
+                                                <th>Reasion for Leave</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <tr>
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                
+                                            </tr>
+                                            
+                                            </tbody>
+                                       </table>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
