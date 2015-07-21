0e9a4ef7118df1b2611acce2dde2bcf50dd85daf
vinay2 <vk324005@gmail.com>
2015-07-21 14:44:34 +0530

    add view_user file in pages

---
+++ application/views/pages/view_user.php
@@ -0,0 +1,46 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                         <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">Register table</h4>
+                                </div>
+                                <div class="panel-body">
+                               <div class="table-responsive">
+                                    <table id="example" class="display table" style="width: 100%; cellspacing: 0;">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Email</th>
+                                                <th>Address</th>
+                                                <th>Telephone Number</th>
+                                                <th>Fax Number</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                
+                                                <th>Name</th>
+                                                <th>Email</th>
+                                                <th>Address</th>
+                                                <th>Telephone Number</th>
+                                                <th>Fax Number</th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <tr>
+                                                <td>vdv</td>
+                                                <td>dfdde</td>
+                                                <td>ddgsgsd</td>
+                                                <td>sdgdsg</td>
+                                                <td>fsgfdsg</td>
+                                            </tr>
+                                        </tbody>
+                                       </table>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
