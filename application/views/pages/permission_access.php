c47c20406abaa4eff788ad72fab25129b1b4edad
vinay2 <vk324005@gmail.com>
2015-07-22 13:23:39 +0530

    add file pages (permission_access.php)

---
+++ application/views/pages/permission_access.php
@@ -0,0 +1,48 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-8 center">
+                            <div class="panel panel-white">
+                                <div class="panel-body">
+                                <form action="" method="" name="">
+									<div class="col-md-6 center">
+                                                            
+                                                           
+															<div class="form-group col-md-12">
+																<label>Permission</label>
+																<select  class="form-control" id="" name="" required="">
+																<option value="0">Select</option>
+																<option value="0">1</option>
+																<option value="0">2</option>
+                                                
+																</select>
+															</div>
+                                                        
+                                     </div>
+							
+								
+                                    <table class="table">
+                                        
+                                        <tbody>
+                                            
+                                            <tr class="success">
+                                                
+                                                <th>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+												&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+												<input type="checkbox" name="" id="">Acess permission</th>
+                                                <th><input type="checkbox" name="" id="">Remove Permission</th>
+                                                
+                                            </tr>
+                                            
+                                        </tbody>
+                                    </table>
+                                
+							
+									<center> <input type="submit" class="btn btn-info" value="Submit"><center>
+									 
+								</form>
+									
+								</div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
