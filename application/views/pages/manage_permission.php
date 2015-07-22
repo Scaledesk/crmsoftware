cfbc6bd1cfcc1bcc36073e79da3d66d856ad98fc
vinay2 <vk324005@gmail.com>
2015-07-22 13:22:49 +0530

    add file in pages (  manage_permission.php)

---
+++ application/views/pages/manage_permission.php
@@ -0,0 +1,45 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-8 center">
+                            				<div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">Manage Permission</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>S No.</th>
+                                                <th>Permission Name</th>
+                                                <th>Action</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            
+                                            <tr class="success">
+                                                <th scope="row">3</th>
+                                                <th>Permission Name</th>
+                                                <th>Action</th>
+                                                
+                                            </tr>
+                                            
+                                        </tbody>
+                                    </table>
+                                </div>
+								<div class="col-md-6 center">
+                                    
+                                        <div class="form-group col-md-12">
+									<form action="" method="">
+
+                                            <label>Add Permission</label>
+                                            <input type="text" class="form-control" id="add_permission" name="add_permission" placeholder="Add Permission" required="" value="<?php echo $h[0]->order_title; ?>">
+                                        
+										</div>
+									<center> <input type="submit" class="btn btn-info" value="Add"><center> 
+									</form>
+								</div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
