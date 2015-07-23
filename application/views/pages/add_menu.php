479b121f9c5d2cc55666e10d3da9128f7d9901b2
vinay2 <vk324005@gmail.com>
2015-07-23 15:50:39 +0530

    add file in pages (add_menu.php)

---
+++ application/views/pages/add_menu.php
@@ -0,0 +1,45 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-8 center">
+                            <div class="panel panel-white">
+                                <div class="panel-body">
+                                
+									<div class="col-md-8 center">
+                                                            
+                                                           
+											<div class="form-group col-md-12">
+								<form action="" method="" name="">
+																<label>Select Top Menu</label>
+																<select  class="form-control" id="" name="" required="">
+																<option value="0">Select</option>
+																<option value="0">1</option>
+																<option value="0">2</option>
+                                                
+																</select>
+											</div>
+												
+											<div class="form-group col-md-12">
+																<label>Menu</label></br>
+															 <input type="text" class="form-control" placeholder="Enter the Menu Name"  id="" name=""></br>
+											
+											
+												
+															<label>link</label></br>
+															<input type="text" class="form-control" placeholder="Enter the Menu Link"  id="" name="">
+                                                </div>
+										
+										<center> <input type="submit" class="btn btn-info" value="Submit"><center>
+                                     </div>
+							
+								
+                                                                  
+							
+									
+									 
+								</form>
+									
+								</div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
