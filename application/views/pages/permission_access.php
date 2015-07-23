c075887e5e755300837de691911ef88852873cc4
vinay2 <vk324005@gmail.com>
2015-07-23 11:20:26 +0530

    edited permission _access file

--- application/views/pages/permission_access.php
+++ application/views/pages/permission_access.php
@@ -4,50 +4,48 @@
                             <div class="panel panel-white">
                                 <div class="panel-body">
 
-									                  <div class="col-md-8 center">
+					               <div class="col-md-8 center">
 
+                                        <div class="form-group col-md-12">
+                                            <form action="" method="post">
+								                <label>Select User</label>
+                              	                    <select  class="form-control"  name="" required="">
+								                        <option value="">Select</option>
+                                                            <?php
+                                                                foreach($h->result() as $row)
+                                                                {
+                                                            ?>
+                                                        <option value="<?php echo $row->user_id; ?>"><?php echo $row->user_name; ?></option>
+                                                                            <?php } ?>
+								                    </select>
+                                        </div>                                 
 
-                                <form action="" method="post">
-																<label>Select User</label>
-                              	<select  class="form-control"  name="" required="">
-																<option value="">Select</option>
-                                <?php
-                                  foreach($h->result() as $row)
-                                      {
-                                     ?>
-                                <option value="<?php echo $row->user_id; ?>"><?php echo $row->user_name; ?></option>
-                                <?php } ?>
-																</select>
-
-
-
+                                        <div class="col-md-12"> 
 
                                             <div class="form-group col-md-6">
-                                                                <label>Access permission</label></br>
-                                                                <?php
-                                                                foreach($k->result() as $row)
-                                                                {
-                                                                 ?>
-                                                             <input type="checkbox" value="<?php echo $row->children_id; ?>"  name=""><?php echo $row->text.'<br/>';
-                                                           } ?>
+                                                <label>Access permission</label></br>
+                                                    <?php
+                                                        foreach($k->result() as $row)
+                                                        {
+                                                            ?>
+                                                            <input type="checkbox" value="<?php echo $row->children_id; ?>"  name=""><?php echo $row->text.'<br/>';
+                                                        } 
+                                                            ?>
                                             </div>
 
                                                 <div class="form-group col-md-6">
                                                             <label>Remove permission</label></br>
                                                             <input type="checkbox"  id="" name="">
-                                                </div>
+                                               </div>
+                                        </div>
 
-                                        </div></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+                                    
 
 
                                         <center><input type="submit" class="btn btn-info" value="Submit"></center>
-
-
-
-
-
-								</form>
-              </div>
+                                </div>
+                                    </form>
+                                    
 								</div>
                             </div>
                         </div>
