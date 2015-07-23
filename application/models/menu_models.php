915be59a311fd2c60c49bf870ea34fbf7537c96f
Javed101 <javedahamad4@gmail.com>
2015-07-23 15:46:39 +0530

    edited menu models

--- application/models/menu_models.php
+++ application/models/menu_models.php
@@ -20,12 +20,13 @@ function menus() {
          if($this->session->userdata('admin_id')=='')
 				 {
 					$uid=$this->session->userdata('user_id');
-					$query=$this->db->query('select * from user_permission where user_id=1');
+					$query=$this->db->query("select * from user_permission where user_id='$uid'");
 					$res=$query->result();
-					$str=$res[0]->page_id;
+					//$str=$res[0]->page_id;
 				  $arr=explode(',',$res[0]->page_id);
+					$l=end($arr);
 						foreach ($arr as $k => $v) {
-							$q=$this->db->query("select * from menu_children where parent_id='$row->parent_id' and children_id BETWEEN '$arr[0]' AND '$arr[4]'");
+							$q=$this->db->query("select * from menu_children where parent_id='$row->parent_id' and children_id BETWEEN '$arr[0]' AND '$l'");
 							if ($q->num_rows() > 0) {
 	              $row->children = $q->result();
 							}
