65ad9289cc24a4a77401da688e3ff200426cc2e3
vinay2 <vk324005@gmail.com>
2015-07-24 18:26:25 +0530

    add file in pages ('add_milestone.php')

---
+++ application/views/pages/add_milestone.php
@@ -0,0 +1,71 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-8 center">
+                            <div class="panel panel-white">
+                                <div class="panel-body">
+                                     <div id="rootwizard">
+                                        <ul class="nav nav-tabs" role="tablist">
+                                            <li role="presentation" class="active"><a href="#tab1" data-toggle="tab"><i class="fa fa-user m-r-xs"></i><b>Product Milestone</b></a></li>
+                                        </ul>
+                          
+                                    
+                                        
+                                        
+                                            <div class="tab-content">
+                                                <div class="tab-pane active fade in" id="tab1">
+                                                    <div class="row m-b-lg">
+                                                        <div class="col-md-12">
+                                                            <div class="row">
+                    <form id="" name="formselct" action="" method="Post">
+                                                        <div class="form-group col-md-12">
+                                
+                                           
+                              <select  class="form-control" id="name" name="select1" onchange="checkValue(this)">
+                                                                <option value="select">Select Product</option>
+                                                                <option value="0">1</option>
+                                                                <option>2</option>
+                                                
+                                                                </select>
+                            </div>
+                                                                
+                                                                
+                                                                <div class="form-group col-md-12">
+                                                                    <label for="exampleInputPassword1">Name</label>
+                                                                    <input type="text" class="form-control" name="" id="" placeholder="Name" required="">
+                                                                </div>
+                                                                <div class="form-group col-md-12">
+                                                                    <label for="exampleInputName">Description</label>
+                                                                    <textarea class="form-control" name="description" id="description" maxlength='500' placeholder="Description" required=""></textarea>
+                                </div>
+                                <div class="form-group col-md-12">
+                                                                    <label>Expected Date</label>
+                                                                    <input type="text" class="form-control date-picker" id="date" name="" placeholder="Expected Date" required="">
+                                                                </div>
+                                <div class="form-group col-md-12">
+                                                                    <label>End Date</label>
+                                                                    <input type="text" class="form-control date-picker" id="date" name="" placeholder="End Date" required="">
+                                                                </div>
+                              
+                            
+                                                    <center><button type="submit" class="btn btn-primary">Add</button></center>
+                                                
+                              </form>
+                                                            </div>
+                              
+                              
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </div>
+                                                
+                                                
+                                                
+                                                
+                                            </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
