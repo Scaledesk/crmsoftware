377df3fc42a6e131dba5f7531c0e94754555d12f
Javed101 <javedahamad4@gmail.com>
2015-07-24 19:12:24 +0530

    edited add milesstone

--- application/views/pages/add_milesstone.php
+++ application/views/pages/add_milesstone.php
@@ -7,10 +7,10 @@
                                         <ul class="nav nav-tabs" role="tablist">
                                             <li role="presentation" class="active"><a href="#tab1" data-toggle="tab"><i class="fa fa-user m-r-xs"></i><b>Product Milestone</b></a></li>
                                         </ul>
-                          
-                                    
-                                        
-                                        
+
+
+
+
                                             <div class="tab-content">
                                                 <div class="tab-pane active fade in" id="tab1">
                                                     <div class="row m-b-lg">
@@ -18,17 +18,17 @@
                                                             <div class="row">
                     <form id="" name="formselct" action="" method="Post">
                                                         <div class="form-group col-md-12">
-                                
-                                           
+
+                                           <label for="exampleInputPassword1">Select Product</label>
                               <select  class="form-control" id="name" name="select1" onchange="checkValue(this)">
                                                                 <option value="select">Select Product</option>
                                                                 <option value="0">1</option>
                                                                 <option>2</option>
-                                                
+
                                                                 </select>
                             </div>
-                                                                
-                                                                
+
+
                                                                 <div class="form-group col-md-12">
                                                                     <label for="exampleInputPassword1">Name</label>
                                                                     <input type="text" class="form-control" name="" id="" placeholder="Name" required="">
@@ -45,24 +45,24 @@
                                                                     <label>End Date</label>
                                                                     <input type="text" class="form-control date-picker" id="date" name="" placeholder="End Date" required="">
                                                                 </div>
-                              
-                            
+
+
                                                     <center><button type="submit" class="btn btn-primary">Add</button></center>
-                                                
+
                               </form>
                                                             </div>
-                              
-                              
+
+
                                                         </div>
-                                                        
+
                                                     </div>
                                                 </div>
-                                                
-                                                
-                                                
-                                                
+
+
+
+
                                             </div>
-                                        
+
                                     </div>
                                 </div>
                             </div>
