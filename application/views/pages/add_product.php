a03bfa0b83dd453616d7dc0e2392612ecd6bfcc8
Javed101 <javedahamad4@gmail.com>
2015-07-24 16:56:04 +0530

    edited add product

--- application/views/pages/add_product.php
+++ application/views/pages/add_product.php
@@ -3,61 +3,60 @@
                         <div class="col-md-8 center">
                             <div class="panel panel-white">
                                 <div class="panel-body">
-                                                              
-                                <center><label><h2>Product Details</h2></label></center>  
-                                <div class="row m-b-lg">
-                                    
-                           
+                                    <?php if($h!='')
+                                    { ?>
+                                      <center><label><h2>Update Product Details</h2></label></center>
+                    <div class="row m-b-lg">
+      	                <form id="wizardForm" action="<?=(base_url().'Product_control/do_edit_product/'.$h[0]->product_id)?>" method="post">
+                                  <?php }
+                                    else{ ?>
+
+                                <center><label><h2>Product Details</h2></label></center>
+              <div class="row m-b-lg">
+	                <form id="wizardForm" action="<?=(base_url().'Product_control/do_add_product')?>" method="post">
+                   <?php } ?>
                                 <div class="col-md-6">
-								<form id="wizardForm" action="" method="post">
-                                    
+
+
                                         <div class="form-group col-md-12">
                                             <label>Name</label>
-                                            <input type="text" class="form-control" id="" name="" placeholder="Name" required="">
+                                            <input type="text" class="form-control" id="" name="product_name" value="<?php echo $h[0]->product_name; ?>" placeholder="Product Name" required="">
                                         </div>
-										<div class="form-group col-md-12">
+									            	        <div class="form-group col-md-12">
                                                                     <label for="exampleInputName">Description</label>
-                                                                    <textarea class="form-control" name="description" id="description" maxlength='500' placeholder="Description" required="" style="margin: 0px -0.5px 0px 0px; height: 100px; width: 308px;"></textarea>
+                                                                    <textarea class="form-control" name="product_description" id="description" maxlength='500' placeholder="Description" required="" style="margin: 0px -0.5px 0px 0px; height: 100px; width: 308px;"><?php echo $h[0]->product_description; ?></textarea>
                                         </div>
-                                        
-                                </div> 
-                                  
 
-
-
-
-
-
-
-
-
+                                </div>
                             <div class="col-md-6">
-								<div class="form-group col-md-12">
+								                        <div class="form-group col-md-12">
                                             <label>Start Date</label>
-                                            <input type="text" class="form-control date-picker" name="" placeholder="Start date" required="">
+                                            <input type="text" class="form-control date-picker" name="product_start_date" value="<?php echo date('d/m/Y',strtotime($h[0]->product_start_date)); ?>" placeholder="Start date" required="">
                                         </div>
-										<div class="form-group col-md-12">
+										                    <div class="form-group col-md-12">
                                             <label>End Date</label>
-                                            <input type="text" class="form-control date-picker" name="" placeholder="End date" required="">
+                                            <input type="text" class="form-control date-picker" name="product_end_date" value="<?php echo date('d/m/Y',strtotime($h[0]->product_end_date)); ?>" placeholder="End date" required="">
                                         </div>
-									<div class="form-group col-md-12">
+									                      <div class="form-group col-md-12">
                                                                     <label>Status Level</label>
-																	<select  class="form-control" id="" name="supplier_name" required="">
+																	                          <select  class="form-control" id="" name="product_status" required="">
                                                                 <option value="select">Select</option>
-                                                                <option>1</option>
-                                                                <option>2</option>
-                                                
+                                                                <?php for ($i=1; $i <= 10; $i++) { ?>
+                                                                <option value="<?php echo $i; ?>" <?php if($i==$h[0]->product_status) echo 'checked'; ?>><?php echo $i; ?></option>
+                                                                <?php } ?>
+
+
                                                                 </select>
                                         </div>
-                                    				 
-							</div>
-								
-								
-                                            
+
+						              	</div>
+
+
+
                               <center><input type="submit" class="btn btn-info" value="Submit"></center>
-                                
-							</form>
-                                        
+
+						        	</form>
+
                         </div>
                                     </div>
                             </div>
