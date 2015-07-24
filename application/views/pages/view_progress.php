fe4b6daeafc65459b7a1ac8098fa87b6d769deac
vinay2 <vk324005@gmail.com>
2015-07-24 18:49:02 +0530

    add file in pages("view_progress.php")

---
+++ application/views/pages/view_progress.php
@@ -0,0 +1,48 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-10 center">
+                         <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">View Progress</h4>
+                                </div>
+                                <div class="panel-body">
+                                   <div class="table-responsive">
+                                    <table id="example" class="display table" style="width: 100%; cellspacing: 0;">
+                                        <thead>
+                                            <tr>
+                                                <th>Product</th>
+                                                <th>Date</th>
+                                                <th>Title</th>
+                                                <th>Description</th>
+                                                <th>Action</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>Product</th>
+                                                <th>Date</th>
+                                                <th>Title</th>
+                                                <th>Description</th>
+                                                <th>Action</th>
+                                                
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <tr>
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011/04/25</td>
+                                                
+                                                
+                                            </tr>
+                                        </tbody>
+                                       </table>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
