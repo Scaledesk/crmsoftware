4e43a4c483bc6661fbd50a18d0275077d33a7676
vinay2 <vk324005@gmail.com>
2015-07-24 15:09:02 +0530

    add file in pages (add_product.php)

---
+++ application/views/pages/add_product.php
@@ -0,0 +1,66 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-8 center">
+                            <div class="panel panel-white">
+                                <div class="panel-body">
+                                                              
+                                <center><label><h2>Product Details</h2></label></center>  
+                                <div class="row m-b-lg">
+                                    
+                           
+                                <div class="col-md-6">
+								<form id="wizardForm" action="" method="post">
+                                    
+                                        <div class="form-group col-md-12">
+                                            <label>Name</label>
+                                            <input type="text" class="form-control" id="" name="" placeholder="Name" required="">
+                                        </div>
+										<div class="form-group col-md-12">
+                                                                    <label for="exampleInputName">Description</label>
+                                                                    <textarea class="form-control" name="description" id="description" maxlength='500' placeholder="Description" required="" style="margin: 0px -0.5px 0px 0px; height: 100px; width: 308px;"></textarea>
+                                        </div>
+                                        
+                                </div> 
+                                  
+
+
+
+
+
+
+
+
+
+                            <div class="col-md-6">
+								<div class="form-group col-md-12">
+                                            <label>Start Date</label>
+                                            <input type="text" class="form-control date-picker" name="" placeholder="Start date" required="">
+                                        </div>
+										<div class="form-group col-md-12">
+                                            <label>End Date</label>
+                                            <input type="text" class="form-control date-picker" name="" placeholder="End date" required="">
+                                        </div>
+									<div class="form-group col-md-12">
+                                                                    <label>Status Level</label>
+																	<select  class="form-control" id="" name="supplier_name" required="">
+                                                                <option value="select">Select</option>
+                                                                <option>1</option>
+                                                                <option>2</option>
+                                                
+                                                                </select>
+                                        </div>
+                                    				 
+							</div>
+								
+								
+                                            
+                              <center><input type="submit" class="btn btn-info" value="Submit"></center>
+                                
+							</form>
+                                        
+                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
