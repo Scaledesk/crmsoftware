6985dc3ddfb517b2c6ebe51a4f6ca79ad8efebdd
vinay2 <vk324005@gmail.com>
2015-07-25 12:00:29 +0530

    add file in pages("view_product_document.php")

---
+++ application/views/pages/view_product_document.php
@@ -0,0 +1,51 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-12 center">
+                         <div class="panel panel-white">
+                                <div class="panel-heading clearfix">
+                                    <h4 class="panel-title">View Milestone</h4>
+                                </div>
+                                <div class="panel-body">
+                                   <div class="table-responsive">
+                                    <table id="example" class="display table" style="width: 100%; cellspacing: 0;">
+                                        <thead>
+                                            <tr>
+                                                <th>S.No</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                                <th>Category</th>
+                                                <th>Upload File</th>
+                                                <th>Action</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>S.No</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                                <th>Category</th>
+                                                <th>Upload File</th>
+                                                <th>Action</th>
+                                                
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <tr>
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011/04/25</td>
+                                                <td>2011/04/25</td>
+                                                
+                                                
+                                            </tr>
+                                        </tbody>
+                                       </table>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
