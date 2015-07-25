8b7e6abbf325fde28c8dc72784ca6125ef50a611
Javed101 <javedahamad4@gmail.com>
2015-07-25 11:46:53 +0530

    edited product document details

--- application/views/pages/add_product_document.php
+++ application/views/pages/add_product_document.php
@@ -7,28 +7,30 @@
                                         <ul class="nav nav-tabs" role="tablist">
                                             <li role="presentation" class="active"><a href="#tab1" data-toggle="tab"><i class="fa fa-user m-r-xs"></i><b>Product Document</b></a></li>
                                         </ul>
-                          
-                                    
-                                        
-                                        
+
+
+
+
                                             <div class="tab-content">
                                                 <div class="tab-pane active fade in" id="tab1">
                                                     <div class="row m-b-lg">
                                                         <div class="col-md-12">
                                                             <div class="row">
-										<form id="" name="formselct" action="" method="Post">
+										<form id="" name="formselct" action="<?=(base_url().'Product_control/do_add_document')?>" method="post" enctype="multipart/form-data">
                                                         <div class="form-group col-md-12">
-																
-                                           
-															<select  class="form-control" id="" name="" onchange="checkValue(this)">
-                                                                <option value="select">Select Product</option>
-                                                                <option value="0">1</option>
-                                                                <option>2</option>
-                                                
-                                                                </select>
+
+
+                                                          <select  class="form-control" id="" name="product_id">
+                                                                                            <option value="select">Select Product</option>
+                                                                                            <?php
+                                                                                            foreach ($h->result() as $row) { ?>
+                                                                                            <option value="<?php echo $row->product_id; ?>"><?php echo $row->product_name;; ?></option>
+                                                                                          <?php   } ?>
+
+                                                          </select>
 														</div>
-                                                                
-                                                                
+
+
                                                                 <div class="form-group col-md-12">
                                                                     <label for="exampleInputPassword1">Name</label>
                                                                     <input type="text" class="form-control" name="" id="" placeholder="Name" required="">
@@ -38,37 +40,37 @@
                                                                     <textarea class="form-control" name="description" id="description" maxlength='500' placeholder="Description" required=""></textarea>
 																</div>
 																<div class="form-group col-md-12">
-																
-                                           
-															<select  class="form-control" id="name" name="select1" onchange="checkValue(this)">
-                                                                <option value="select">Select Category</option>
-                                                                <option value="0">1</option>
-                                                                <option>2</option>
-                                                
-                                                                </select>
+                                  <select  class="form-control" id="" name="document_category_id">
+                                                                    <option value="select">Select categary</option>
+                                                                    <?php
+                                                                    foreach ($k->result() as $row) { ?>
+                                                                    <option value="<?php echo $row->document_category_id; ?>"><?php echo $row->document_category_name; ?></option>
+                                                                  <?php   } ?>
+
+                                    </select>
 														</div>
 																<div class="form-group col-md-12">
                                                                     <label>Upload File</label>
-                                                                    <input type="file" class="form-control date-picker" id="date" name="" placeholder="Upload File" required="">
+                                                                    <input type="file" class="form-control" id="date" name="" placeholder="Upload File" required="">
                                                                 </div>
-															
-														
+
+
                                                     <center><button type="submit" class="btn btn-primary">Add</button></center>
-                                                
+
 															</form>
                                                             </div>
-															
-															
+
+
                                                         </div>
-                                                        
+
                                                     </div>
                                                 </div>
-                                                
-                                                
-                                                
-                                                
+
+
+
+
                                             </div>
-                                        
+
                                     </div>
                                 </div>
                             </div>
