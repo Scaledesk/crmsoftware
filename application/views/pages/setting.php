9d5231eb0fcfedc4a37fc814592fe37b344e1199
Javed101 <javedahamad4@gmail.com>
2015-08-07 19:35:25 +0530

    added setting page

---
+++ application/views/pages/setting.php
@@ -0,0 +1,47 @@
+<div id="main-wrapper" class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-white">
+                                <div class="panel-body">
+                                   
+
+                                    <div class="panel panel-white">
+                                    <div class="panel-heading clearfix">
+                                        <h3 class="panel-title">Left tabs</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="tabs-left" role="tabpanel">
+                                            <!-- Nav tabs -->
+                                            <ul class="nav nav-tabs" role="tablist">
+                                                <li role="presentation" class="active"><a href="#tab9" role="tab" data-toggle="tab">Tab 1</a></li>
+                                                <li role="presentation"><a href="#tab10" role="tab" data-toggle="tab">Tab 2</a></li>
+                                                <li role="presentation"><a href="#tab11" role="tab" data-toggle="tab">Tab 3</a></li>
+                                                <li role="presentation"><a href="#tab12" role="tab" data-toggle="tab">Tab 4</a></li>
+                                            </ul>
+                                            <!-- Tab panes -->
+                                            <div class="tab-content">
+                                                <div role="tabpanel" class="tab-pane active fade in" id="tab9">
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat.</p>
+                                                       
+                                                </div>
+                                                <div role="tabpanel" class="tab-pane fade" id="tab10">
+                                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                                                </div>
+                                                <div role="tabpanel" class="tab-pane fade" id="tab11">
+                                                    <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+                                                </div>
+                                                <div role="tabpanel" class="tab-pane fade" id="tab12">
+                                                    <p>Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div> 
+                                    
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
