95fed9a119d0882fec514d763a8fd0e80771fe04
Javed101 <javedahamad4@gmail.com>
2015-08-07 16:06:20 +0530

    added page for upload profile picture

--- application/views/pages/uploadProfilePicture.php
+++ application/views/pages/uploadProfilePicture.php
@@ -6,11 +6,11 @@
                             <div class="login-box">
                                 <a href="index.html" class="logo-name text-lg text-center">Upload Profile Picture</a>
                                  <form class="m-t-md" action="<?=(base_url().'User_control/do_upload_image')?>" enctype="multipart/form-data" method="post">
-
                                     <div class="form-group">
-                                        <input type="file" class="form-control" name="profile_pic" placeholder="profile picture" required="">
+                                        <input type="file" class="form-control" name="image" id="image" onchange="readUrl()" placeholder="profile picture" required="">
                                     </div>
                                     <div class="form-group">
+                                      <img src="" id="img_id" height="100px" width="100px"/>
                                     </div>
                                     <button type="submit" class="btn btn-primary btn-block">Upload</button>
 
@@ -22,3 +22,24 @@
                 </div><!-- Main Wrapper -->
             </div><!-- Page Inner -->
         </main><!-- Page Content -->
+<<script type="text/javascript">
+
+function readUrl()
+{
+  if(document.getElementById('image').value!='')
+   {
+     var imgid=document.getElementById('image');
+	 var freader=new FileReader();
+	 freader.readAsDataURL(imgid.files[0]);
+	 freader.onloadend=function(event)
+	 {
+	   var show_id=document.getElementById('img_id');
+	   //alert(event.target.result);
+	   show_id.src=event.target.result;
+	 }
+   }
+
+
+}
+
+</script>
